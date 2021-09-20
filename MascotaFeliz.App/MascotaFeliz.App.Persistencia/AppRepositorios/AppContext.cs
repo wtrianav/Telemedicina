4b1cfa49200a36417cea99996cb70713ebf5bbcd
Andŕes Triana <wtriana@outlook.com>
2021-09-20 07:51:04 -0500

    Proyecto compilado y conectado con la base de datos

---
+++ MascotaFeliz.App/MascotaFeliz.App.Persistencia/AppRepositorios/AppContext.cs
@@ -0,0 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+using MascotaFeliz.App.Dominio;
+
+namespace MascotaFeliz.App.Persistencia {
+    
+    public class AppContext : DbContext {
+
+        public DbSet<Persona> Personas { get; set; }
+    
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+        if (!optionsBuilder.IsConfigured) {
+            optionsBuilder.UseSqlServer("Data Source = (localdb)\\MSSQLLocalDB; Initial Catalog = MascotaFeliz.Data");
+        }
+
+    }
+
+    }
+}
